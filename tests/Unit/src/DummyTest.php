0867d962006baae5c5720cfe05597617fcb57755
brambaud <beram.job@gmail.com>
2019-02-11 21:37:52 +0100

    test travis-ci

---
+++ tests/Unit/src/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ekino\Drupal\Debug\Tests\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testShouldFail()
+    {
+        $this->assertTrue(false);
+    }
+}
